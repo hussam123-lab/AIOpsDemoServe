996945bad555b1d5a7bc1dd0b2de5a88f8d127a5
hussam123-lab <62464644+hussam123-lab@users.noreply.github.com>
2021-12-21 16:38:22 +1100

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ SECRET_KEY = os.urandom(32)
 ev_calculator_app = Flask(__name__)
 ev_calculator_app.config['SECRET_KEY'] = SECRET_KEY
 
-# methods=['GET', 'POST']
-@ev_calculator_app.route('/',methods=['GET', 'POST'])
+
+@ev_calculator_app.route('/', methods=['GET', 'POST'])
 def operation_result():
     # request.form looks for:
     # html tags with matching "name="
@@ -66,5 +66,4 @@ def flash_errors(form):
 
 
 if __name__ == '__main__':
-# print(__name__)
     ev_calculator_app.run()
